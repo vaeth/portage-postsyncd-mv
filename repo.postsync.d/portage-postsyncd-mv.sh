d89ee476df960fcbe4fab20cd0b32a56d38ddacf
Martin Väth <martin@mvath.de>
2016-10-16 09:09:34 +0200

    Use git udpate-index --refresh -q --unmerged

--- repo.postsync.d/portage-postsyncd-mv.sh
+++ repo.postsync.d/portage-postsyncd-mv.sh
@@ -505,10 +505,13 @@ git_pull() {
 	git_pull_remote=`git -C "$local_path" rev-parse --abbrev-ref --symbolic-full-name '@{upstream}'` \
 		|| return
 	# Use the same strategy as portage: git fetch && git reset --merge
-	# We need to call git status first to make e.g. git+overlayfs happy,
-	# see https://bugs.gentoo.org/show_bug.cgi?id=552814
-	git_pull_status_state=0
-	git -C "$local_path" status >/dev/null 2>&1 || git_pull_status_state=$?
+	# We need to call git update-index first to make e.g. git+overlayfs
+	# happy, see https://bugs.gentoo.org/show_bug.cgi?id=552814
+	# The -q --unmerged are both necessary here to not error out early
+	# (they are used internally by git status).
+	git_pull_index_state=0
+	git -C "$local_path" update-index --refresh -q --unmerged \
+		|| git_pull_index_state=$?
 	git -C "$local_path" fetch $option_quiet \
 		${git_pull_depth:+--depth=$git_pull_depth} \
 		"${git_pull_remote%%/*}" \
@@ -516,7 +519,7 @@ git_pull() {
 	git -C "$local_path" reset --merge "$git_pull_remote" $option_quiet
 	git_pull_merge_state=$?
 	[ $git_pull_merge_state -eq 0 ] || return $git_pull_merge_state
-	return $git_pull_status_state
+	return $git_pull_index_state
 }
 
 # Usage: git_new depth remote local_path [Description]
